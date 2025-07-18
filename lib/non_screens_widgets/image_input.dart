7119762873c4513101f7aebed5b21df125c68841
Shivansh <shivanshh.1602@gmail.com>
2025-07-18 23:53:32 +0530

    camera accessible

--- lib/non_screens_widgets/image_input.dart
+++ lib/non_screens_widgets/image_input.dart
@@ -1,9 +1,12 @@
 //holds the image input logic ,, alllows user to open device camera show a preview of the image that was taken , pass the image back to the add place screen to be added to the place that is being created
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ImageInput extends StatefulWidget {
-  ImageInput({super.key});
+  const ImageInput({super.key});
+
   @override
   State<ImageInput> createState() {
     return _ImageInput();
@@ -11,8 +14,35 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInput extends State<ImageInput> {
+  File? _selectedImage;
+
+  void _takePicture() async {
+    // mehtod to open camera and click the image
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+    _selectedImage = File( pickedImage.path,); // changed the XFILE type of pcikedimage to normal File type and stored it in _selected image which of type File ,, here the File object stores the path of Xfile picked image
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content=TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+        onPressed: _takePicture,
+      );
+
+      if(_selectedImage!=null ){
+        content= Image.file(_selectedImage!,fit: BoxFit.fill,);
+      }
+
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -25,11 +55,7 @@ class _ImageInput extends State<ImageInput> {
       width: double.infinity,
       alignment: Alignment
           .center, //allligns the child widget which is button in the centre of the container
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-        onPressed: () {},
-      ),
+      child:content 
     );
   }
 }
