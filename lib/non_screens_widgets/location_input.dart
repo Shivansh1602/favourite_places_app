e8b86e466771ea32d8ca5610ce947918f7d6e506
Shivansh <shivanshh.1602@gmail.com>
2025-07-20 23:42:17 +0530

    Enabled location access & integrated Google Maps API with basic debugging

--- lib/non_screens_widgets/location_input.dart
+++ lib/non_screens_widgets/location_input.dart
@@ -1,9 +1,11 @@
 // to input location
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
+
   @override
   State<LocationInput> createState() {
     return _LocationInputState();
@@ -11,32 +13,78 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false; // made to show a loading spinner 
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+       _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent=Text(
+            'No location ',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          );
+
+          if(_isGettingLocation==true){
+            previewContent=const Center(child:CircularProgressIndicator());
+          }
+
+
+
     return Column(
       children: [
         Container(
-          height: 170,
+          height: 140,
           width: double.infinity,
           decoration: BoxDecoration(
             border: Border.all(
               width: 1,
-              color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+              color:
+                  Theme.of(context).colorScheme.primary.withAlpha(51), // withValues fixed to withAlpha
             ),
           ),
-          child: Text(
-            'No location ',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
+          child: previewContent// to show the loading spinner   
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               label: Text('Get Current Location'),
               icon: Icon(Icons.location_on),
             ),
