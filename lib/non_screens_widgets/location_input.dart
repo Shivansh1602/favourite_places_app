ec200efcc8a9c60b0411e6cbe78484fda8f4712b
Shivansh <shivanshh.1602@gmail.com>
2025-07-24 23:24:04 +0530

    Added manual location selection with Google Maps, preview snapshot, and reverse geocoding support

--- lib/non_screens_widgets/location_input.dart
+++ lib/non_screens_widgets/location_input.dart
@@ -2,16 +2,17 @@
 import 'dart:convert'; //provides json
 
 import 'package:favourite_places_app/models/place.dart';
+import 'package:favourite_places_app/screens/map_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
-  const LocationInput({super.key,required this.onSelectLocation});
+  const LocationInput({super.key, required this.onSelectLocation});
 
-  final void Function(PlaceLocation location) onSelectLocation;
-
+  final void Function(PlaceLocation location) onSelectLocation;// to communicate to the parent widget which is addd screen here   
 
   @override
   State<LocationInput> createState() {
@@ -20,17 +21,41 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  PlaceLocation? _pickedLocation;//  to store the location (lat,lng,address)
+  PlaceLocation? _pickedLocation; //  to store the location (lat,lng,address)
   var _isGettingLocation = false; // made to show a loading spinner
 
-  String get locationImage{// getter to generate the snapsot and  the preveiw of the location using api 
-  if(_pickedLocation==null){
-    return '';
-  }
-   final lat=_pickedLocation!.latitude;
-   final lng=_pickedLocation!.longitude; 
+  String get locationImage {
+    // getter to generate the snapsot and  the preveiw of the location using api
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyC2uLjIwQjaxrMhWR2_yrdyRdweZWkPquA';
   }
+
+void _savePlace(double latitude, double longitude )async {
+   
+    final url = Uri.parse(
+      "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=AIzaSyC2uLjIwQjaxrMhWR2_yrdyRdweZWkPquA",
+    );
+
+    final response = await http.get(url);
+
+    final resdata = json.decode(response.body);
+    final address =
+        resdata['results'][0]['formatted_address']; // human readable address by converting longitude and latitude in human readable address using google maps api reverse geocoding
+
+    setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: latitude,
+        longitude: longitude,
+        address: address,
+      );
+      _isGettingLocation = false;
+    });
+    widget.onSelectLocation(_pickedLocation!);
+}
 
   void _getCurrentLocation() async {
     Location location = Location();
@@ -65,47 +90,42 @@ class _LocationInputState extends State<LocationInput> {
     if (lat == null || lng == null) {
       return;
     }
+    
+    _savePlace(lat, lng);
+  } //
 
-    final url = Uri.parse(
-      "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyC2uLjIwQjaxrMhWR2_yrdyRdweZWkPquA",
-    );
+  void _selectOnMap()async {
+   final pickedLocation=await  Navigator.of(  context,).push<LatLng>(
+      MaterialPageRoute(
+        builder: (ctx) =>const  MapScreen()));
 
-    final response = await http.get(url);
-
-    final resdata = json.decode(response.body);
-    final address =
-        resdata['results'][0]['formatted_address']; // human readable address by converting longitude and latitude in human readable address using google maps api reverse geocoding
-
-    setState(() {
-      _pickedLocation = PlaceLocation(
-        latitude: lat,
-        longitude: lng,
-        address: address,
-      );
-      _isGettingLocation = false;
-    });
-    widget.onSelectLocation(_pickedLocation!);
+        if(pickedLocation==null){
+          return;
+        }
+        _savePlace(pickedLocation.latitude, pickedLocation.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget previewContent = 
-    Text( 'No location ',
+    Widget previewContent = Text(
+      'No location ',
       textAlign: TextAlign.center,
       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
         color: Theme.of(context).colorScheme.onSurface,
       ),
     );
 
-    if(_pickedLocation!=null){
-      previewContent=Image.network(locationImage,fit: BoxFit.cover,width: double.infinity,height: double.infinity,);
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
     }
 
     if (_isGettingLocation == true) {
-    
-         previewContent = const Center(child: CircularProgressIndicator());
-     
-     
+      previewContent = const Center(child: CircularProgressIndicator());
     }
 
     return Column(
@@ -132,7 +152,9 @@ class _LocationInputState extends State<LocationInput> {
               icon: Icon(Icons.location_on),
             ),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                _selectOnMap();
+              },
               label: Text('Select on Map'),
               icon: Icon(Icons.map),
             ),
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,4 @@
-// to show the set location on map by opening the map and letting user set location manually 
+// to show the set location on map by opening the map and letting user set location manually
 
 import 'package:favourite_places_app/models/place.dart';
 import 'package:flutter/material.dart';
@@ -8,8 +8,8 @@ class MapScreen extends StatefulWidget {
   const MapScreen({
     super.key,
     this.location = const PlaceLocation(
-      latitude: 37.22,
-      longitude: -122.084,
+      latitude: 37.4220,
+      longitude: -122.0841,
       address: '',
     ),
     this.isSelecting = true,
@@ -25,6 +25,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,20 +35,36 @@ class _MapScreenState extends State<MapScreen> {
         ),
         actions: [
           if (widget.isSelecting)
-            IconButton(onPressed: () {}, icon: const Icon(Icons.save)),
+            IconButton(
+             onPressed: () {
+              Navigator.of(context).pop(_pickedLocation);
+             },
+             icon: const Icon(Icons.save)),
         ],
       ),
       body: GoogleMap(
+        onTap: widget.isSelecting==false?null:(postion) {
+          setState(() {
+            _pickedLocation = postion;
+          });
+        },
         initialCameraPosition: CameraPosition(
           target: LatLng(widget.location.latitude, widget.location.longitude),
           zoom: 16,
         ),
-        markers: {
-          Marker(markerId:const MarkerId('m1'),
-          position: LatLng(widget.location.latitude, widget.location.longitude)
-          ),
-          
-        }//set data type,
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: //_pickedLocation!=null? _pickedLocation!:// or
+                      _pickedLocation ?? // dart checks double question mark as u want this value if it is not null otherwise the following value
+                      LatLng(
+                        widget.location.latitude,
+                        widget.location.longitude,
+                      ),
+                ),
+              }, //set data type,
       ),
     );
   }
